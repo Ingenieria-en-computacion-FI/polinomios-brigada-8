36dcde6368230fa2cbf363b24877dcf8f247b116
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-05-08 20:19:20 -0600

    Update polinomio.h

--- src/polinomio.h
+++ src/polinomio.h
@@ -1,1 +1,38 @@
-#include "polinomio.h"
+#ifndef __POLINOMIO_H__
+#define __POLINOMIO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+typedef struct Termino{
+    float coeficiente;
+    int exponente;
+} Term;
+
+typedef struct Nodo {
+    Term termino;
+    struct Nodo* siguiente;
+    struct Nodo* anterior;
+} Nodo;
+
+typedef struct {
+    Nodo* cabeza;
+    Nodo* cola;
+} Polinomio;
+
+
+Nodo* crearNodo(float coef, int exp);
+void insertarNodoOrdenado(Polinomio* p, Nodo* nuevo);
+void eliminarNodo(Polinomio* p, int exp);
+Polinomio* crearPolinomio();
+void insertarTermino(Polinomio* p, float coef, int exp);
+float evaluarPolinomio(Polinomio* p, float x);
+Polinomio* sumarPolinomios(Polinomio* p1, Polinomio* p2);
+Polinomio* multiplicarPolinomios(Polinomio* p1,Polinomio* p2);
+char* polinomioToString(Polinomio* p);
+void destruirPolinomio(Polinomio* p);
+
+#endif
